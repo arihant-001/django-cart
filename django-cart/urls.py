051f71e7437d6b797468719cb752cf8661c60b0b
Arihant Jain <arihant.jain.iitr@gmail.com>
2020-08-30 01:32:56 +0530

    refactoring static files

--- django-cart/urls.py
+++ django-cart/urls.py
@@ -12,3 +12,5 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
